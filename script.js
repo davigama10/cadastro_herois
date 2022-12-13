0da6601909138f29f4ea3ce37be59034f7a9f3dd
DAVI MOREIRA LIMA DA GAMA <davigama10@gmail.com>
2022-12-13 15:27:41 -0300

    Primeira versão da página de cadastro simples de heróis

---
+++ script.js
@@ -0,0 +1,38 @@
+class item {
+    constructor(name, strenght) {
+        this.name = name;
+        this.strenght = strenght;
+    }
+}
+
+var arr = [];
+
+
+function salvar(){
+    var aux = new item(document.getElementById('hero_name').value, document.getElementById('hero_strenght').value);
+    arr.push(aux);  
+    arr.forEach(function (bora){
+        console.log(bora);
+    })  
+    document.getElementById('hero_name').value = "";
+    document.getElementById('hero_strenght').value = "";
+}
+
+
+function carregar(){
+
+    let resultDIV = document.getElementById('d');
+    resultDIV.innerHTML = "";
+
+    arr.forEach(function(opa){
+        var text = 'nome do herói: ' + opa.name + ' força: ' + opa.strenght;
+        let p = document.createElement("p");
+        p.innerHTML = text;
+        resultDIV.append(p);
+
+    })
+}
+
+
+
+
